6e70b13328d97bbfa610b7a164ebba133711cde9
RudaMazurek <fasoladanadmucholada@gmail.com>
2023-11-15 10:27:57 +0100

    Rozkład

---
+++ rozkład/czynniki pierwsze.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    int k = 2;
+    cout << "Podaj liczbe: ";
+    cin >> n;
+    while (n > 1)
+    {
+        while (n % k == 0) {
+            cout << k << " ";
+            n = n / k;
+        }
+        k++;
+    }
+}
